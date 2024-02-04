cafcb3b0ff29db88a3b162ca69941efbd97aa398
tylerstennett <tyler.j.stennett@gmail.com>
2024-02-04 18:17:41 -0500

    Added exam database functionality

--- app/src/main/java/com/example/collegescheduler/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/logout/LogoutFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -50,7 +51,7 @@ public class LogoutFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         welcomeHeader = view.findViewById(R.id.welcome_header);
