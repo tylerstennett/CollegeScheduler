d0aebe0e4d05086bf67e989f048298fab34d0719
nninanguyenn <Ninanguyen159@gmail.com>
2024-02-05 18:22:58 -0500

    pushed

--- app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentAdapter.java
+++ app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentAdapter.java
@@ -1,68 +1,115 @@
 package com.example.collegescheduler.ui.assignments;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.collegescheduler.R;
 import com.example.collegescheduler.db.entities.Assignment;
-import java.util.List;
+import com.example.collegescheduler.ui.assignments.AssignmentsFragment;
 
+import java.util.List;
 public class AssignmentAdapter extends RecyclerView.Adapter<AssignmentAdapter.AssignmentViewHolder> {
     private List<Assignment> list;
     private AssignmentsFragment assignmentsFragment;
+    private Assignment selectedItem; // Track the selected item
+    private TextView infoAssignment;
+    private EditListener editListener;
+    private Context context;
 
-    public AssignmentAdapter(List<Assignment> list) {
-        this.list = list;
-    }
+    public AssignmentAdapter(List<Assignment> list, EditListener editListener) {
+            this.list = list;
+            this.editListener = editListener;
+        }
 
-    // creates new exam views
-    @Override
-    public AssignmentViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        // Create a new Exam view
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.assignment_add_on, viewGroup, false);
-        // return ViewHolder with new assignmentView
-        return new AssignmentViewHolder(view);
-    }
+        // creates new exam views
+        @Override
+        public AssignmentViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            // Create a new Exam view
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.assignment_add_on, viewGroup, false);
+            // return ViewHolder with new assignmentView
 
-    // replace contents of a view
-    @Override
-    public void onBindViewHolder(AssignmentViewHolder viewHolder, final int position) {
-        viewHolder.assignmentDueDate.setText("Due Date: " + list.get(position).dueDate);
-        viewHolder.assignmentClass.setText("Class: " + list.get(position).courseName);
-        viewHolder.assignmentAssignment.setText("Assignment: " + list.get(position).assignmentName);
-    }
+            return new AssignmentViewHolder(view);
+        }
 
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
+        // replace contents of a view
+        @Override
+        public void onBindViewHolder(AssignmentViewHolder viewHolder, int position) {
 
-    public void addItem(Assignment newAssignment) {
-        list.add(newAssignment);
-        notifyItemInserted(list.size());
-    }
+            viewHolder.assignmentDueDate.setText("Due Date: " + list.get(position).dueDate);
+            viewHolder.assignmentClass.setText("Class: " + list.get(position).courseName);
+            viewHolder.assignmentAssignment.setText("Assignment: " + list.get(position).assignmentName);
 
-    //sorting attempt
+            viewHolder.assignmentDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Assignment removedItem = list.get(viewHolder.getAbsoluteAdapterPosition());
+                    list.remove(viewHolder.getAbsoluteAdapterPosition());
+                    notifyItemRemoved(viewHolder.getAbsoluteAdapterPosition());
+                }
+
+            });
+
+            viewHolder.assignmentEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = viewHolder.getAbsoluteAdapterPosition();
+                    editListener.onEditButtonClick(position);
+                }
+
+            });
+
+        }
+
+        public void editItem(Assignment newAssignment, int position) {
+            list.set(position, newAssignment);
+            notifyItemChanged(position);
+        }
+
+        public interface EditListener {
+            void onEditButtonClick(int position);
+        }
+
+            @Override
+        public int getItemCount() {
+            return list.size();
+        }
+        public void addItem(Assignment newAssignment) {
+            list.add(newAssignment);
+            notifyItemInserted(list.size());
+        }
+        //sorting attempt
 //    public void setSortField(String selectedSortField) {
 //    }
 
-    public static class AssignmentViewHolder extends RecyclerView.ViewHolder {
-
-        private final TextView assignmentDueDate;
-        private final TextView assignmentClass;
-        private final TextView assignmentAssignment;
-        private final View view;
 
 
-        public AssignmentViewHolder (View itemView) {
-            super(itemView);
+        public static class AssignmentViewHolder extends RecyclerView.ViewHolder {
 
-            assignmentDueDate = itemView.findViewById(R.id.textViewDueDate);
-            assignmentClass = itemView.findViewById(R.id.textViewAssociatedClass);
-            assignmentAssignment = itemView.findViewById(R.id.textViewAssignment);
-            view = itemView;
+            private final TextView assignmentDueDate;
+            private final TextView assignmentClass;
+            private final TextView assignmentAssignment;
+            private final Button assignmentDelete;
+            private final Button assignmentEdit;
+
+
+            private final View view;
+
+
+            public AssignmentViewHolder (View itemView) {
+                super(itemView);
+                assignmentDueDate = itemView.findViewById(R.id.textViewDueDate);
+                assignmentClass = itemView.findViewById(R.id.textViewAssociatedClass);
+                assignmentAssignment = itemView.findViewById(R.id.textViewAssignment);
+
+                assignmentDelete = itemView.findViewById(R.id.buttonDeleteTask);
+                assignmentEdit = itemView.findViewById(R.id.buttonEditTask);
+
+                view = itemView;
+            }
+
         }
     }
-}
--- app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsFragment.java
+++ app/src/main/java/com/example/collegescheduler/ui/assignments/AssignmentsFragment.java
@@ -1,6 +1,5 @@
 package com.example.collegescheduler.ui.assignments;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
@@ -17,43 +17,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.collegescheduler.R;
 import com.example.collegescheduler.db.entities.Assignment;
+import com.example.collegescheduler.ui.assignments.AssignmentAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AssignmentsFragment extends Fragment {
-
-    private EditText editAssignment;
-    private EditText editTextDueDate;
-    private EditText editClassName;
+public class AssignmentsFragment extends Fragment implements AssignmentAdapter.EditListener {
+    private EditText assignmentEntry;
+    private EditText dueDateEntry;
+    private EditText classEntry;
     private ConstraintLayout inputContainer;
     private AssignmentAdapter assignmentAdapter;
     private RecyclerView recyclerViewAssignments;
     List<Assignment> list;
+    private Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_assignments, container, false);
-        View assignmentAddOnView = inflater.inflate(R.layout.assignment_add_on, container, false);
-
 
         Spinner assignmentSortSpinner = view.findViewById(R.id.assignmentSort);
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(),
                 R.array.assignmentSort, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         assignmentSortSpinner.setAdapter(adapter);
 
-
-        // Initialize views
-        editAssignment = view.findViewById(R.id.editTextAssignment);
-        editTextDueDate = view.findViewById(R.id.editTextDueDate);
-        editClassName = view.findViewById(R.id.editTextClass);
-
+        assignmentEntry = view.findViewById(R.id.editTextAssignment);
+        dueDateEntry = view.findViewById(R.id.editTextDueDate);
+        classEntry = view.findViewById(R.id.editTextClass);
         inputContainer = view.findViewById(R.id.inputContainer);
         recyclerViewAssignments = view.findViewById(R.id.recyclerViewAssignments);
 
@@ -61,7 +54,7 @@ public class AssignmentsFragment extends Fragment {
         recyclerViewAssignments.setLayoutManager(layoutManager);
 
         list = new ArrayList<>();
-        assignmentAdapter = new AssignmentAdapter(list);
+        assignmentAdapter = new AssignmentAdapter(list, this);
         recyclerViewAssignments.setAdapter(assignmentAdapter);
 
         Button addButton = view.findViewById(R.id.addButton);
@@ -69,22 +62,6 @@ public class AssignmentsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 onAddButtonClick();
-            }
-        });
-
-        Button editButton = assignmentAddOnView.findViewById(R.id.buttonEditTask);
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onEditButtonClick();
-            }
-        });
-
-        Button deleteButton = assignmentAddOnView.findViewById(R.id.buttonDeleteTask);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onDeleteButtonClick();
             }
         });
 
@@ -96,37 +73,45 @@ public class AssignmentsFragment extends Fragment {
 //                showSortOptionsDialog(); // You can implement a dialog to select the sort field
 //            }
 //        });
-
         return view;
     }
 
-    private void onAddButtonClick() {
-        String assignmentDueDate = editTextDueDate.getText().toString();
-        String assignmentClass = editClassName.getText().toString();
-        String assignmentAssignment = editAssignment.getText().toString();
+        public void onEditButtonClick ( int position){
+            // Get text from input fields
+            String assignmentDueDate = dueDateEntry.getText().toString();
+            String assignmentClass = classEntry.getText().toString();
+            String assignmentAssignment = assignmentEntry.getText().toString();
 
-        // create new exam card
+            Assignment assignment = new Assignment(assignmentDueDate, assignmentClass, assignmentAssignment);
 
-        Assignment assignment = new Assignment(assignmentDueDate,assignmentClass,assignmentAssignment);
+            assignmentAdapter.editItem(assignment, position);
 
-        // recyclerViewExams.addView(assignmentView);
-        assignmentAdapter.addItem(assignment);
+            resetInputs();
+        }
 
-        // clear input fields
-        editTextDueDate.getText().clear();
-        editClassName.getText().clear();
-        editAssignment.getText().clear();
-    }
+        private void onAddButtonClick () {
+            String assignmentDueDate = dueDateEntry.getText().toString();
+            String assignmentClass = classEntry.getText().toString();
+            String assignmentAssignment = assignmentEntry.getText().toString();
 
-    private void onEditButtonClick() {
-    }
+            if (assignmentDueDate.isEmpty() || assignmentClass.isEmpty() || assignmentAssignment.isEmpty()) {
+                Toast.makeText(requireContext(), "All fields must be filled", Toast.LENGTH_SHORT).show();
+                return;
+             }
+            Assignment assignment = new Assignment(assignmentDueDate, assignmentClass, assignmentAssignment);
+            assignmentAdapter.addItem(assignment);
+            resetInputs();
+        }
 
-    private void onDeleteButtonClick() {
-    }
+        private void resetInputs () {
+            // clear input fields
+            dueDateEntry.getText().clear();
+            classEntry.getText().clear();
+            assignmentEntry.getText().clear();
+        }
 
 
-
-    //sorting attempt
+            //sorting attempt
 //    private void showSortOptionsDialog() {
 //        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
 //        builder.setTitle("Sort By");
@@ -161,4 +146,3 @@ public class AssignmentsFragment extends Fragment {
 //                return null;
 //        }
 }
-
