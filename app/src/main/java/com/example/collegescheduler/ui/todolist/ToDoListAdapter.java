86c973d46ac5d8d5a97b087dc8de330bb7eb617c
tylerstennett <tyler.j.stennett@gmail.com>
2024-02-06 10:51:52 -0500

    Updated db version

--- app/src/main/java/com/example/collegescheduler/ui/todolist/ToDoListAdapter.java
+++ app/src/main/java/com/example/collegescheduler/ui/todolist/ToDoListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.collegescheduler.ui.todolist;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
